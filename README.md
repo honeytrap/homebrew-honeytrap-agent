90d0d43048296aa29abc963867c97d78f9e7597d
Remco Verhoef <remco@dutchcoders.io>
2018-02-26 22:22:55 +0100

    added launch daemon

--- README.md
+++ README.md
@@ -9,3 +9,26 @@ Homebrew formula for honeytrap-agent.
 ```bash
 $ brew install honeytrap/stable/honeytrap-agent
 ```
+
+## Configuration
+
+Edit the file `/usr/local/etc/honeytrap-agent` and configure the server and remote-key.
+
+## Install LaunchDaemon
+
+```
+sudo cp /usr/local/Cellar/honeytrap-agent/agent/homebrew.mxcl.honeytrap-agent.plist /Library/LaunchDaemons/
+sudo chown root:wheel /Library/LaunchDaemons/homebrew.mxcl.honeytrap-agent.plist
+```
+
+### Loading the Agent
+
+```
+launchctl load -w /Library/LaunchDaemons/homebrew.mxcl.honeytrap-agent.plist
+```
+
+### Unloading the Agent
+
+```
+launchctl unload -w /Library/LaunchDaemons/homebrew.mxcl.honeytrap-agent.plist
+```
--- honeytrap-agent.rb
+++ honeytrap-agent.rb
@@ -17,11 +17,55 @@ class HoneytrapAgent < Formula
     clipath = buildpath/"src/github.com/honeytrap/honeytrap-agent"
     clipath.install Dir["*"]
 
+    ldflags = "-X github.com/honeytrap/honeytrap-agent/server.Version=2018-02-26T21:03:09Z -X github.com/honeytrap/honeytrap-agent/server.ReleaseTag=DEVELOPMENT.2018-02-26T21-03-09Z -X github.com/honeytrap/honeytrap-agent/server.CommitID=f2a7ba72b30ba4c8b22f63663958e6eb0dca183b -X github.com/honeytrap/honeytrap-agent/server.ShortCommitID=f2a7ba72b30b"
+
     cd clipath do
-      system "go", "build", "-o", buildpath/"honeytrap-agent"
+      system "go", "build", "-ldflags", ldflags, "-o", buildpath/"honeytrap-agent"
     end
 
     bin.install "honeytrap-agent"
+
+    (buildpath/"honeytrap-agent.toml").write <<~EOS
+      server="{server-name-or-ip:1337}"
+      remote-key="{token}"
+    EOS
+
+    etc.install "honeytrap-agent.toml"
+  end
+
+  def post_install
+    (var/"log/honeytrap-agent").mkpath
+  end
+
+  plist_options :manual => "honeytrap-agent"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>UserName</key>
+      <string>root</string>
+      <key>KeepAlive</key>
+      <true/>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{bin}/honeytrap-agent</string>
+        <string>--config=#{etc}/honeytrap-agent/config.toml</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>WorkingDirectory</key>
+      <string>#{HOMEBREW_PREFIX}</string>
+      <key>StandardErrorPath</key>
+      <string>#{var}/log/honeytrap-agent/output.log</string>
+      <key>StandardOutPath</key>
+      <string>#{var}/log/honeytrap-agent/output.log</string>
+    </dict>
+    </plist>
+    EOS
   end
 
   test do
